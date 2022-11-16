9eca714bfeb074c50afb2a1d3a1ef97727c97933
chaeeunjun <102653631+chaeeunjun@users.noreply.github.com>
2022-11-16 12:13:19 +0900

    애니메이션 구성

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.SmallHeart)
+        basic.showIcon(IconNames.Chessboard)
+        basic.showIcon(IconNames.Diamond)
+    }
+})
+input.onButtonPressed(Button.B, function () {
 	
 })
+basic.showString("merry christmas")
